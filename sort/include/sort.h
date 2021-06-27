7304fb7ee759678bd11e70611fe9451de8274a36
andrfranklin <andrew.frank146@gmail.com>
2021-06-27 21:56:09 +0000

    function sort done

--- sort/include/sort.h
+++ sort/include/sort.h
@@ -1,6 +1,7 @@
 #ifndef GRAAL_H
 #define GRAAL_H
 
+using namespace std;
 #include <utility>
 using std::pair;
 #include <iterator>
@@ -12,11 +13,26 @@ namespace graal {
 
 /*! 
  * TODO: documentação no estilo doxygen
+ * @tparam ForwardIt iterator para o range.
+ * @tparam Comparison o tipo de uma função bool(const T &a, const T &b)
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param cmp A função de comparação que retorna true quando o primeiro parâmetro é menor do que o segundo
+ *
+ * @return A função não retorna nada.
  */
 template<class ForwardIt, class Comparison>
 void sort(ForwardIt first, ForwardIt last, Comparison cmp)
 {
-    // TODO
+    // TODO	
+		for(ForwardIt i = first; i != last; i++){
+				for(ForwardIt j = first; j != last; j++){
+					if(cmp(*i, *j) == true){
+						iter_swap(i, j);
+					}
+				}
+		}
 }
 
 }

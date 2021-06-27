1fcd0fc0fa6bfde40668be733a898b4918d4e1ef
andrfranklin <andrew.frank146@gmail.com>
2021-06-27 20:11:07 +0000

    function equal done

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -1,6 +1,8 @@
 #ifndef GRAAL_H
 #define GRAAL_H
 
+#include <iostream>
+using namespace std;
 #include <utility>
 using std::pair;
 #include <iterator>
@@ -17,7 +19,17 @@ template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
 {
     // TODO
-    return false;
+		int count = 0;
+		bool all = true;
+		for(InputIt1 i = first1; i != last1; i++){
+			InputIt2 k = first2 + count;
+				if(eq(*i, *k) == false){
+					all = false;
+				}
+			count++;
+		}
+    return all;
+		// return false;
 }
 
 template<class InputIt1, class InputIt2, class Equal>
@@ -26,7 +38,16 @@ bool equal(InputIt1 first1, InputIt1 last1,
            Equal eq )
 {
     // TODO
-    return false;
+		bool all = true;
+		while(first1 != last1){
+			if(eq(*first1, *first2)== false){
+				all = false;
+			}
+			first1 = first1 + 1;
+			first2 = first2 + 1;
+		}
+    return all;
+		// return false;
 }
 
 }

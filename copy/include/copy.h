5bc09c8ed9efdf5a4928c485b7ee228a751aa339
andrfranklin <andrew.frank146@gmail.com>
2021-06-27 03:56:58 +0000

    documentation updated

--- copy/include/copy.h
+++ copy/include/copy.h
@@ -12,6 +12,13 @@ namespace graal {
 
 /*! 
  * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param d_first Ponteiro para a primeria posição do primeiro elemento do segundo range
+ *
+ * @return Um InputIt para o ultimo elemento do segundo range
  */
 template<class InputIt>
 InputIt copy(InputIt first, InputIt last, InputIt d_first)

b6798faa5a3631f28faf1e7d833d1857ee62eae1
andrfranklin <andrew.frank146@gmail.com>
2021-06-27 05:27:16 +0000

    documentation updated

--- reverse/include/reverse.h
+++ reverse/include/reverse.h
@@ -1,6 +1,8 @@
 #ifndef GRAAL_H
 #define GRAAL_H
 
+#include <vector>
+using namespace std;
 #include <utility>
 using std::pair;
 #include <iterator>
@@ -12,6 +14,12 @@ namespace graal {
 
 /*! 
  * TODO: documentação no estilo doxygen
+ * @tparam BidirIt iterator para o range.
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ *
+ * @return O range com a ordem de seus elementos invertida.
  */
 template<class BidirIt>
 void reverse(BidirIt first, BidirIt last){

f4dcbf4903c6bbcc9b2458bd25fb822e43911c83
andrfranklin <andrew.frank146@gmail.com>
2021-06-27 20:37:31 +0000

    function find done

--- find/include/find.h
+++ find/include/find.h
@@ -12,6 +12,14 @@ namespace graal {
 
 /*! 
  * TODO: documentação no estilo doxygen
+ * @tparam InputIt ,iterator para o range.
+ * @tparam T ,valor para comparacao.
+ *
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param Equal Função de comparacao.
+ *
+ * @return iterator do elemento comparado se for igual ao valor T ou last se nenhum elemento for igual ao valor T.
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
